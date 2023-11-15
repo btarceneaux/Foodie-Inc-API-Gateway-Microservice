0141acea6b4f46227e9a01df8a9f73b02b1350e2
Burton Arceneaux <btarceneaux@gmail.com>
2023-11-15 12:53:50 -0800

    Initial commit.

---
+++ src/main/java/inc/foodie/FoodieIncApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package inc.foodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieIncApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieIncApiGatewayServiceApplication.class, args);
+	}
+
+}
